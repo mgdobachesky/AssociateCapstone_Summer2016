08eda9d9be02638b975ade6c06e8325c46349268
mike91dobyy <mike91doby@gmail.com>
2016-08-08 08:51:52 -0400

    add libraries

--- cms/views/cmsHeader.php
+++ cms/views/cmsHeader.php
@@ -4,11 +4,8 @@
     <title>Bubba Lyrics - CMS</title>  
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <script type="text/javascript" src="/bubbaLyrics/cms/scripts/jquery.js"></script>
+	<link rel="stylesheet" type="text/css" href="/bubbaLyrics/cms/styles/bootstrap.css">
 	<link rel="stylesheet" type="text/css" href="/bubbaLyrics/cms/styles/stylesheet.css">
 </head>
 <body>
