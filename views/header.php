b48f4df5c58be40ec4d826662c90f2b79d78df57
tcass <tcass@email.neit.edu>
2016-08-28 14:35:46 -0400

    got canvas element to display on page, still have to edit background color and position of canvas

--- views/header.php
+++ views/header.php
@@ -15,7 +15,8 @@
 
 <body>
 	<script type="text/javascript" src="/bubbaLyrics/scripts/headerScript.js"></script>
-		
+	<script type="text/javascript" src="/bubbaLyrics/scripts/canvas.js"></script>
+	
 	<div class="container-fluid">
 		<div class="row">
 			<div class="col-md-12">
@@ -50,14 +51,22 @@
 					</div>           
 				</nav>
 				<br />
-				<br />				
+				<br />	
+
 				<div class="jumbotron">
+				
 					<div class="container-fluid center">
 						<a id="fireGlyph" href="/bubbaLyrics/index.php"><span class="glyphicon glyphicon-fire"></span></a>
+						
 						<h1 id="mainHeader" class="papyrus">Bubba Lyrics</h1>
 						<p class="papyrus">Your go to source for the songs you love!</p>
+						<canvas id="canvas" width="1800" height="100"></canvas>
+     				 <img id="musicNote" width="0" height="0" src="/bubbaLyrics/images/musicNote.jpeg" alt="Music Note">
+						
+						
 					</div>
 				</div>
+
 			</div>
 		</div>
 	</div>
